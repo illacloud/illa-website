f714ade1e9e178dbf165132d513fc5864238639e
Scenery <1507337624@qq.com>
2023-12-28 19:31:44 +0800

    feat: init homepage

--- src/components/home/Index/constants.tsx
+++ src/components/home/Index/constants.tsx
@@ -1,8 +1,3 @@
-import ImageGenerate from "@site/static/img/home3/index/imageGenerate.svg"
-import ImageGenerate2 from "@site/static/img/home3/index/imageGenerate2.svg"
-import ImageGenerateBg from "@site/static/img/home3/index/imageGenerateBg.svg"
-import ImageGenerateBg2 from "@site/static/img/home3/index/imageGenerateBg2.svg"
-
 export const TAB_OPTIONS = [
   {
     label: "Image generator",
@@ -28,32 +23,82 @@ export const TAB_OPTIONS = [
 
 export const SOLUTIONS_IMAGE = [
   {
-    content: <ImageGenerate className="w-full" />,
-    contentBg: <ImageGenerateBg className="w-full" />,
+    content: (
+      <img
+        src="https://cdn.illacloud.com/illa-website/test/imageGenerate.svg"
+        className="w-full"
+      />
+    ),
+    contentBg: (
+      <img
+        src="https://cdn.illacloud.com/illa-website/test/imageGenerateBg.svg"
+        className="w-full"
+      />
+    ),
     appID: "ILAcx4p1C7W3",
     value: 0,
   },
   {
-    content: <ImageGenerate2 className="w-full" />,
-    contentBg: <ImageGenerateBg2 className="w-full" />,
+    content: (
+      <img
+        src="https://cdn.illacloud.com/illa-website/test/imageGenerate2.svg"
+        className="w-full"
+      />
+    ),
+    contentBg: (
+      <img
+        src="https://cdn.illacloud.com/illa-website/test/imageGenerateBg2.svg"
+        className="w-full"
+      />
+    ),
     appID: "ILAcx4p1C7W3",
     value: 1,
   },
   {
-    content: <ImageGenerate className="w-full" />,
-    contentBg: <ImageGenerateBg className="w-full" />,
+    content: (
+      <img
+        src="https://cdn.illacloud.com/illa-website/test/imageGenerate.svg"
+        className="w-full"
+      />
+    ),
+    contentBg: (
+      <img
+        src="https://cdn.illacloud.com/illa-website/test/imageGenerateBg.svg"
+        className="w-full"
+      />
+    ),
     appID: "ILAcx4p1C7W3",
     value: 2,
   },
   {
-    content: <ImageGenerate2 className="w-full" />,
-    contentBg: <ImageGenerateBg2 className="w-full" />,
+    content: (
+      <img
+        src="https://cdn.illacloud.com/illa-website/test/imageGenerate2.svg"
+        className="w-full"
+      />
+    ),
+    contentBg: (
+      <img
+        src="https://cdn.illacloud.com/illa-website/test/imageGenerateBg2.svg"
+        className="w-full"
+      />
+    ),
     appID: "ILAcx4p1C7W3",
     value: 3,
   },
   {
-    content: <ImageGenerate className="w-full" />,
-    contentBg: <ImageGenerateBg className="w-full" />,
+    content: (
+      <img
+        src="https://cdn.illacloud.com/illa-website/test/imageGenerate.svg"
+        className="w-full"
+      />
+    ),
+    contentBg: (
+      <img
+        src="https://cdn.illacloud.com/illa-website/test/imageGenerateBg.svg"
+        className="w-full"
+      />
+    ),
     appID: "ILAcx4p1C7W3",
     value: 4,
   },
@@ -62,27 +107,52 @@ export const SOLUTIONS_IMAGE = [
 export const MOBILE_SOLUTIONS_IMAGE = [
   {
     label: "Image generator",
-    content: <ImageGenerateBg className="w-full" />,
+    content: (
+      <img
+        src="https://cdn.illacloud.com/illa-website/test/imageGenerateBg.svg"
+        className="w-full"
+      />
+    ),
     value: 0,
   },
   {
     label: "AI Voice Generator",
-    content: <ImageGenerateBg2 className="w-full" />,
+    content: (
+      <img
+        src="https://cdn.illacloud.com/illa-website/test/imageGenerateBg.svg"
+        className="w-full"
+      />
+    ),
     value: 1,
   },
   {
     label: "Dashboard",
-    content: <ImageGenerateBg className="w-full" />,
+    content: (
+      <img
+        src="https://cdn.illacloud.com/illa-website/test/imageGenerateBg.svg"
+        className="w-full"
+      />
+    ),
     value: 2,
   },
   {
     label: "CMS",
-    content: <ImageGenerateBg2 className="w-full" />,
+    content: (
+      <img
+        src="https://cdn.illacloud.com/illa-website/test/imageGenerateBg2.svg"
+        className="w-full"
+      />
+    ),
     value: 3,
   },
   {
     label: "CRM",
-    content: <ImageGenerateBg className="w-full" />,
+    content: (
+      <img
+        src="https://cdn.illacloud.com/illa-website/test/imageGenerateBg.svg"
+        className="w-full"
+      />
+    ),
     value: 4,
   },
 ]
