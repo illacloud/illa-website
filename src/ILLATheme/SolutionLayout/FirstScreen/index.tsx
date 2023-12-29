8a15151a399a47a7e954f2b454635cc433a73114
Scenery <1507337624@qq.com>
2023-12-29 14:26:24 +0800

    feat: update soltuions fork

--- src/ILLATheme/SolutionLayout/FirstScreen/index.tsx
+++ src/ILLATheme/SolutionLayout/FirstScreen/index.tsx
@@ -3,12 +3,13 @@ import { useUtmParams } from "@site/src/hooks/useUtmParams"
 import { sendTagEvent } from "@site/src/utils/gtag"
 import { usePaintBg } from "@site/src/hooks/usePaintBg"
 import style from "./index.module.css"
-import Translate, { translate } from "@docusaurus/Translate"
-import { CLOUD_URL } from "@site/src/constants/url"
+import Translate from "@docusaurus/Translate"
 import LinkButton from "@site/src/components/common/LinkButton"
 import Link from "@docusaurus/Link"
 import Interpolate from "@docusaurus/Interpolate"
 import { motion, useScroll, useTransform } from "framer-motion"
+import { addUrlParams } from "@site/src/utils/urlParams"
+import { CLOUD_URL, DEMO_BASE_URL } from "@site/src/constants/url"
 
 interface IFirstScreenProps {
   slogan: string
@@ -16,7 +17,7 @@ interface IFirstScreenProps {
   description: string
   colorStart: string
   colorEnd: string
-  href: string
+  appID: string
   image: string
 }
 
@@ -26,7 +27,7 @@ const FirstScreen: FC<IFirstScreenProps> = ({
   description,
   colorStart,
   colorEnd,
-  href,
+  appID,
   image,
 }) => {
   const getUtmParams = useUtmParams()
@@ -38,6 +39,16 @@ const FirstScreen: FC<IFirstScreenProps> = ({
   const scale = useTransform(scrollYProgress, [0, 0.002], [0.95, 1])
 
   usePaintBg(canvasRef, containerRef)
+
+  const handleDemoHref = (appID: string) => {
+    const redirectURL = addUrlParams(`${DEMO_BASE_URL}/${appID}/detail`, {
+      operateType: "fork",
+    })
+    const url = getUtmParams(
+      `${CLOUD_URL}?redirectURL=${encodeURIComponent(redirectURL)}`,
+    )
+    return url
+  }
 
   return (
     <div ref={containerRef} className={style.containerStyle}>
@@ -75,6 +86,22 @@ const FirstScreen: FC<IFirstScreenProps> = ({
             }}
           >
             <img src={image} width="100%" alt="" />
+            <div className={style.linkButtonStyle}>
+              <LinkButton
+                href={handleDemoHref(appID)}
+                size="small"
+                colorType="dark"
+                handleClick={() => {
+                  sendTagEvent({
+                    action: "solution_try_for_free",
+                  })
+                }}
+              >
+                <Translate id="website_4.solution.use_this_template">
+                  Try demo now!
+                </Translate>
+              </LinkButton>
+            </div>
           </motion.div>
           <img className="lg:hidden block w-full" src={image} alt="" />
         </div>
