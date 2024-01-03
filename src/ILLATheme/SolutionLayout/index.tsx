635c155f7d8c99bdda72f43b9fc0050749645273
Scenery <1507337624@qq.com>
2024-01-03 18:55:45 +0800

    feat: solutions add schema data

--- src/ILLATheme/SolutionLayout/index.tsx
+++ src/ILLATheme/SolutionLayout/index.tsx
@@ -7,16 +7,45 @@ import { useBlogPost } from "@docusaurus/theme-common/internal"
 import Head from "@docusaurus/Head"
 import CommBottom from "@site/src/components/common/CommBottom"
 import FirstScreen from "./FirstScreen"
+import {
+  ADMIN_PANEL_SCHEMA_DATA,
+  AI_VOICE_GENERATOR_SCHEMA_DATA,
+  CMS_SCHEMA_DATA,
+  CRM_SCHEMA_DATA,
+  DASHBOARD_SCHEMA_DATA,
+  IMAGE_GENERATE_SCHEMA_DATA,
+} from "@site/src/config/Solutions"
+
+const getSchemaData = (path: string) => {
+  const prefix = /solutions-/
+  if (!prefix.test(path)) return
+  const pageName = path.split(prefix)[1]
+  switch (pageName) {
+    case "image-generator":
+      return IMAGE_GENERATE_SCHEMA_DATA
+    case "ai-voice-generator":
+      return AI_VOICE_GENERATOR_SCHEMA_DATA
+    case "dashboard":
+      return DASHBOARD_SCHEMA_DATA
+    case "cms":
+      return CMS_SCHEMA_DATA
+    case "crm":
+      return CRM_SCHEMA_DATA
+    case "admin-panel":
+      return ADMIN_PANEL_SCHEMA_DATA
+  }
+}
 
 export const SolutionLayout = (props: Record<string, any>) => {
   const { children } = props
-  const { frontMatter: metaProps }: any = useBlogPost()
-
+  const { frontMatter: metaProps, metadata }: any = useBlogPost()
+  const schemaData = getSchemaData(metadata.permalink)
   return (
     <>
       <Head>
         <meta name="twitter:title" content={metaProps.title} />
         <meta name="twitter:description" content={metaProps.description} />
+        <script type="application/ld+json">{JSON.stringify(schemaData)}</script>
       </Head>
       <CommonLayout {...metaProps}>
         <div className="w-full overflow-visible  bg-black relative z-[1]">
--- src/config/Solutions/index.ts
+++ src/config/Solutions/index.ts
@@ -1,1 +1,397 @@
-// export const
+export const IMAGE_GENERATE_SCHEMA_DATA = {
+  "@context": "https://schema.org",
+  "@graph": [
+    {
+      "@type": "Organization",
+      name: "ILLA Cloud",
+      url: "https://www.illacloud.com/#organization",
+      logo: "https://www.illacloud.com/#logo",
+      sameAs: [
+        "https://twitter.com/illacloudhq",
+        "https://github.com/illacloud/illa-builder",
+        "https://www.illacloud.com/",
+        "https://www.youtube.com/@illacloud",
+        "https://www.linkedin.com/company/illacloud/",
+      ],
+    },
+    {
+      "@type": "WebPage",
+      url: "https://www.illacloud.com/solutions-image-generator",
+      name: "Customize AI Image Generator for your business with ILLA Cloud ",
+      description:
+        "Text-to-Image, transform your imaginative ideas into stunning visuals. Image-to-image, enhance your images or drafts by applying artistic styles or modifying image content based on your description.",
+    },
+    {
+      "@type": "HowTo",
+      name: "Customize AI Image Generator for your business",
+      description:
+        "Text-to-Image, transform your imaginative ideas into stunning visuals. Image-to-image, enhance your images or drafts by applying artistic styles or modifying image content based on your description.",
+      image:
+        "https://cdn.illacloud.com/official-website/img/solutions/ai-image1.svg",
+      estimatedCost: {
+        "@type": "MonetaryAmount",
+        currency: "",
+        value: "",
+      },
+      tool: {
+        "@type": "HowToTool",
+        name: "ILLA Cloud",
+      },
+      step: [
+        {
+          "@type": "HowToStep",
+          text: "Design interface in minites by dragging and dropping the components",
+          image:
+            "https://cdn.illacloud.com/official-website/img/solutions/cms/design-in-minutes.svg",
+          name: "Design interface in minites by dragging and dropping the components",
+          url: "https://www.illacloud.com/solutions-image-generator",
+        },
+        {
+          "@type": "HowToStep",
+          text: "Connect to APIs to generate images such as text to image, draft to image, change the style of images...",
+          image:
+            "https://cdn.illacloud.com/official-website/img/solutions/draft-to-image.svg",
+          name: "Connect to APIs to generate images",
+          url: "https://www.illacloud.com/solutions-image-generator",
+        },
+      ],
+    },
+  ],
+}
+
+export const AI_VOICE_GENERATOR_SCHEMA_DATA = {
+  "@context": "https://schema.org",
+  "@graph": [
+    {
+      "@type": "Organization",
+      name: "ILLA Cloud",
+      url: "https://www.illacloud.com/#organization",
+      logo: "https://www.illacloud.com/#logo",
+      sameAs: [
+        "https://twitter.com/illacloudhq",
+        "https://github.com/illacloud/illa-builder",
+        "https://www.illacloud.com/",
+        "https://www.youtube.com/@illacloud",
+        "https://www.linkedin.com/company/illacloud/",
+      ],
+    },
+    {
+      "@type": "WebPage",
+      url: "https://www.illacloud.com/solutions-ai-voice-generator",
+      name: "Build AI Voice Generator for your business",
+      description:
+        "Speech-to-text, transcribe the audio into text and translate or summarize it. Text-to-speech, create natural AI voices instantly in any language and let your content go beyond text. ",
+    },
+    {
+      "@type": "HowTo",
+      name: "Build AI Voice Generator for your business",
+      description:
+        "Speech-to-text, transcribe the audio into text and translate or summarize it. Text-to-speech, create natural AI voices instantly in any language and let your content go beyond text.",
+      image:
+        "https://cdn.illacloud.com/official-website/img/solutions/audio/audio-cover.svg",
+      estimatedCost: {
+        "@type": "MonetaryAmount",
+        currency: "",
+        value: "",
+      },
+      tool: {
+        "@type": "HowToTool",
+        name: "ILLA Cloud",
+      },
+      step: [
+        {
+          "@type": "HowToStep",
+          text: "Design interface in minites by dragging and dropping the components",
+          image:
+            "https://cdn.illacloud.com/official-website/img/solutions/cms/design-in-minutes.svg",
+          name: "Design interface in minites by dragging and dropping the components",
+          url: "https://www.illacloud.com/solutions-ai-voice-generator",
+        },
+        {
+          "@type": "HowToStep",
+          text: "Connect to APIs to transcribe the audio into text in any language or create natural AI voices instantly in any language.",
+          image:
+            "https://cdn.illacloud.com/official-website/img/solutions/audio/meeting.svg",
+          name: "Connect to APIs to transcribe the audio into text in any language or create natural AI voices instantly in any language.",
+          url: "https://www.illacloud.com/solutions-ai-voice-generator",
+        },
+      ],
+    },
+  ],
+}
+
+export const DASHBOARD_SCHEMA_DATA = {
+  "@context": "https://schema.org",
+  "@graph": [
+    {
+      "@type": "Organization",
+      name: "ILLA Cloud",
+      url: "https://www.illacloud.com/#organization",
+      logo: "https://www.illacloud.com/#logo",
+      sameAs: [
+        "https://twitter.com/illacloudhq",
+        "https://github.com/illacloud/illa-builder",
+        "https://www.illacloud.com/",
+        "https://www.youtube.com/@illacloud",
+        "https://www.linkedin.com/company/illacloud/",
+      ],
+    },
+    {
+      "@type": "WebPage",
+      url: "https://www.illacloud.com/solutions-dashboard",
+      name: "Customize Data Analysis Dashboard for Your Business",
+      description:
+        "Build comprehensive data analysis dashboards and drive your business growth with minimal investment.",
+    },
+    {
+      "@type": "HowTo",
+      name: "Build Data Analysis Dashboard in minutes",
+      description:
+        "Build comprehensive data analysis dashboards and drive your business growth with minimal investment.",
+      image:
+        "https://cdn.illacloud.com/official-website/img/solutions/dashboard/cover.svg",
+      estimatedCost: {
+        "@type": "MonetaryAmount",
+        currency: "",
+        value: "",
+      },
+      tool: {
+        "@type": "HowToTool",
+        name: "ILLA Cloud",
+      },
+      step: [
+        {
+          "@type": "HowToStep",
+          text: "Design interface in minites by dragging and dropping the components",
+          image:
+            "https://cdn.illacloud.com/official-website/img/solutions/cms/design-in-minutes.svg",
+          name: "Design interface in minites by dragging and dropping the components",
+          url: "https://www.illacloud.com/solutions-dashboard",
+        },
+        {
+          "@type": "HowToStep",
+          text: "Connect to your data sources",
+          image:
+            "https://cdn.illacloud.com/official-website/img/solutions/cms/data-source.svg",
+          name: "Connect to your data sources",
+          url: "https://www.illacloud.com/solutions-dashboard",
+        },
+        {
+          "@type": "HowToStep",
+          text: "Analysis with AI",
+          image:
+            "https://cdn.illacloud.com/official-website/img/solutions/dashboard/analysis.gif",
+          name: "Analysis with AI",
+          url: "https://www.illacloud.com/solutions-dashboard",
+        },
+      ],
+    },
+  ],
+}
+
+export const CMS_SCHEMA_DATA = {
+  "@context": "https://schema.org",
+  "@graph": [
+    {
+      "@type": "Organization",
+      name: "ILLA Cloud",
+      url: "https://www.illacloud.com/#organization",
+      logo: "https://www.illacloud.com/#logo",
+      sameAs: [
+        "https://twitter.com/illacloudhq",
+        "https://github.com/illacloud/illa-builder",
+        "https://www.illacloud.com/",
+        "https://www.youtube.com/@illacloud",
+        "https://www.linkedin.com/company/illacloud/",
+      ],
+    },
+    {
+      "@type": "WebPage",
+      url: "https://www.illacloud.com/solutions-image-generator",
+      name: "Build CMS and operate more efficiently",
+      description:
+        "Built content management system for efficient operations. AI-powered, integrate with AI capabilities, enabling you to efficiently create both visual and textual content. File management, support file storage and delivering, simplifying the content management workflow.",
+    },
+    {
+      "@type": "HowTo",
+      name: "Build CMS in minutes",
+      description:
+        "Built content management system for efficient operations. AI-powered, integrate with AI capabilities, enabling you to efficiently create both visual and textual content. File management, support file storage and delivering, simplifying the content management workflow.",
+      image:
+        "https://cdn.illacloud.com/official-website/img/solutions/cms/cover.svg",
+      estimatedCost: {
+        "@type": "MonetaryAmount",
+        currency: "",
+        value: "",
+      },
+      tool: {
+        "@type": "HowToTool",
+        name: "ILLA Cloud",
+      },
+      step: [
+        {
+          "@type": "HowToStep",
+          text: "Design interface in minites by dragging and dropping the components",
+          image:
+            "https://cdn.illacloud.com/official-website/img/solutions/cms/design-in-minutes.svg",
+          name: "Design interface in minites by dragging and dropping the components",
+          url: "https://www.illacloud.com/solutions-cms",
+        },
+        {
+          "@type": "HowToStep",
+          text: "Connect to your data sources",
+          image:
+            "https://cdn.illacloud.com/official-website/img/solutions/cms/data-source.svg",
+          name: "Connect to your data sources",
+          url: "https://www.illacloud.com/solutions-cms",
+        },
+        {
+          "@type": "HowToStep",
+          text: "Generate content such as images, descriptions, etc with AI",
+          image:
+            "https://cdn.illacloud.com/official-website/img/solutions/cms/ai.gif",
+          name: "Generate content such as images, descriptions, etc with AI",
+          url: "https://www.illacloud.com/solutions-cms",
+        },
+      ],
+    },
+  ],
+}
+
+export const CRM_SCHEMA_DATA = {
+  "@context": "https://schema.org",
+  "@graph": [
+    {
+      "@type": "Organization",
+      name: "ILLA Cloud",
+      url: "https://www.illacloud.com/#organization",
+      logo: "https://www.illacloud.com/#logo",
+      sameAs: [
+        "https://twitter.com/illacloudhq",
+        "https://github.com/illacloud/illa-builder",
+        "https://www.illacloud.com/",
+        "https://www.youtube.com/@illacloud",
+        "https://www.linkedin.com/company/illacloud/",
+      ],
+    },
+    {
+      "@type": "WebPage",
+      url: "https://www.illacloud.com/solutions-crm",
+      name: "Better sales CRM solution to close deals faster",
+      description:
+        "Efficiently manage all your leads, customer relationships, sales pipeline, and tasks in one place, so you can streamline your sales process and focus on closing the deals.",
+    },
+    {
+      "@type": "HowTo",
+      name: "Better sales CRM solution to close deals faster",
+      description:
+        "Efficiently manage all your leads, customer relationships, sales pipeline, and tasks in one place, so you can streamline your sales process and focus on closing the deals.",
+      image:
+        "https://cdn.illacloud.com/official-website/img/solutions/crm/cover.svg",
+      estimatedCost: {
+        "@type": "MonetaryAmount",
+        currency: "",
+        value: "",
+      },
+      tool: {
+        "@type": "HowToTool",
+        name: "ILLA Cloud",
+      },
+      step: [
+        {
+          "@type": "HowToStep",
+          text: "Design interface in minites by dragging and dropping the components",
+          image:
+            "https://cdn.illacloud.com/official-website/img/solutions/cms/design-in-minutes.svg",
+          name: "Design interface in minites by dragging and dropping the components",
+          url: "https://www.illacloud.com/solutions-crm",
+        },
+        {
+          "@type": "HowToStep",
+          text: "Connect to your data sources",
+          image:
+            "https://cdn.illacloud.com/official-website/img/solutions/cms/data-source.svg",
+          name: "Connect to your data sources",
+          url: "https://www.illacloud.com/solutions-crm",
+        },
+        {
+          "@type": "HowToStep",
+          text: "Generate marketing content such as emails, SMS, etc with AI",
+          image:
+            "https://cdn.illacloud.com/official-website/img/solutions/crm/send.gif",
+          name: "Generate marketing content such as emails, SMS, etc with AI",
+          url: "https://www.illacloud.com/solutions-crm",
+        },
+      ],
+    },
+  ],
+}
+
+export const ADMIN_PANEL_SCHEMA_DATA = {
+  "@context": "https://schema.org",
+  "@graph": [
+    {
+      "@type": "Organization",
+      name: "ILLA Cloud",
+      url: "https://www.illacloud.com/#organization",
+      logo: "https://www.illacloud.com/#logo",
+      sameAs: [
+        "https://twitter.com/illacloudhq",
+        "https://github.com/illacloud/illa-builder",
+        "https://www.illacloud.com/",
+        "https://www.youtube.com/@illacloud",
+        "https://www.linkedin.com/company/illacloud/",
+      ],
+    },
+    {
+      "@type": "WebPage",
+      url: "https://www.illacloud.com/solutions-admin-panel",
+      name: "Build Customized Admin Panel for Website and Apps",
+      description:
+        "Build admin panels at 10x speed to manage data or perform operations and allow developers to focus on business development. ",
+    },
+    {
+      "@type": "HowTo",
+      name: "Build Customized Admin Panel for Website and Apps",
+      description:
+        "Build admin panels at 10x speed to manage data or perform operations and allow developers to focus on business development.",
+      image:
+        "https://cdn.illacloud.com/official-website/img/solutions/dashboard/cover.svg",
+      estimatedCost: {
+        "@type": "MonetaryAmount",
+        currency: "",
+        value: "",
+      },
+      tool: {
+        "@type": "HowToTool",
+        name: "ILLA Cloud",
+      },
+      step: [
+        {
+          "@type": "HowToStep",
+          text: "Design interface in minites by dragging and dropping the components",
+          image:
+            "https://cdn.illacloud.com/official-website/img/solutions/cms/design-in-minutes.svg",
+          name: "Design interface in minites by dragging and dropping the components",
+          url: "https://www.illacloud.com/solutions-admin-panel",
+        },
+        {
+          "@type": "HowToStep",
+          text: "Connect to your data sources",
+          image:
+            "https://cdn.illacloud.com/official-website/img/solutions/cms/data-source.svg",
+          name: "Connect to your data sources",
+          url: "https://www.illacloud.com/solutions-admin-panel",
+        },
+        {
+          "@type": "HowToStep",
+          text: "Analysis with AI",
+          image:
+            "https://cdn.illacloud.com/official-website/img/solutions/dashboard/analysis.gif",
+          name: "Analysis with AI",
+          url: "https://www.illacloud.com/solutions-admin-panel",
+        },
+      ],
+    },
+  ],
+}
