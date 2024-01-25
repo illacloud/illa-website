b09478e8cf94ab5ecb6c61b65049b72e9fcf0e62
Scenery <1507337624@qq.com>
2024-01-25 19:50:25 +0800

    feat: update previous & next style

--- src/theme/BlogListPaginator/index.js
+++ src/theme/BlogListPaginator/index.js
@@ -1,42 +1,52 @@
-import React from 'react';
-import Translate, {translate} from '@docusaurus/Translate';
-import PaginatorNavLink from '@theme/PaginatorNavLink';
+import React from "react"
+import Translate, { translate } from "@docusaurus/Translate"
+import Link from "@docusaurus/Link"
+import NextIcon from "@site/static/img/public/nextIcon.svg"
+import PreviousIcon from "@site/static/img/public/previousIcon.svg"
 export default function BlogListPaginator(props) {
-  const {metadata} = props;
-  const {previousPage, nextPage} = metadata;
+  const { metadata } = props
+  const { previousPage, nextPage } = metadata
   return (
     <nav
-      className="pagination-nav"
+      className="w-full grid grid-cols-2 pt-[16px]"
       aria-label={translate({
-        id: 'theme.blog.paginator.navAriaLabel',
-        message: 'Blog list page navigation',
-        description: 'The ARIA label for the blog pagination',
-      })}>
+        id: "theme.blog.paginator.navAriaLabel",
+        message: "Blog list page navigation",
+        description: "The ARIA label for the blog pagination",
+      })}
+    >
       {previousPage && (
-        <PaginatorNavLink
-          permalink={previousPage}
-          title={
+        <label className="col-start-1 flex justify-start">
+          <Link
+            to={previousPage}
+            className=" text-tech-purple-03 px-[16px] py-[6px] flex justify-center items-center gap-[8px] text-[16px] font-[500] leading-[28px] rounded-[24px] hover:bg-tech-purple-03/[0.2] no-underline hover:no-underline"
+          >
+            <PreviousIcon width="16px" />
             <Translate
               id="theme.blog.paginator.newerEntries"
-              description="The label used to navigate to the newer blog posts page (previous page)">
+              description="The label used to navigate to the newer blog posts page (previous page)"
+            >
               Newer Entries
             </Translate>
-          }
-        />
+          </Link>
+        </label>
       )}
       {nextPage && (
-        <PaginatorNavLink
-          permalink={nextPage}
-          title={
+        <label className="col-start-2 flex justify-end">
+          <Link
+            to={nextPage}
+            className=" text-tech-purple-03 px-[16px] py-[6px] flex justify-center items-center gap-[8px] text-[16px] font-[500] leading-[28px] rounded-[24px] hover:bg-tech-purple-03/[0.2] no-underline hover:no-underline"
+          >
             <Translate
               id="theme.blog.paginator.olderEntries"
-              description="The label used to navigate to the older blog posts page (next page)">
+              description="The label used to navigate to the older blog posts page (next page)"
+            >
               Older Entries
             </Translate>
-          }
-          isNext
-        />
+            <NextIcon width="16px" />
+          </Link>
+        </label>
       )}
     </nav>
-  );
+  )
 }
